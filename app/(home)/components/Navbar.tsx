f3d5afb1d0f3158aa6b553ce2abc187f452d04c6
YLamouddan <165455306+YLamouddan@users.noreply.github.com>
2024-04-01 13:13:46 +0200

    First commit after creating the Repo installed npm & nvm

---
+++ app/(home)/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Navbar() {
+
+//const with links and labels and icons for linkedin, twitter, github, and one for cv
+
+const socials = [
+{
+    link :  "www.linkedin.com/in/iyassir",
+
+    Label: "Linkedin",
+
+    icon: "",
+},
+{
+    link : "https://github.com/YLamouddan",
+
+    Label: "Github",
+
+    icon: "",
+},
+{
+    link : "https://twitter.com/YLamouddan",
+
+    Label: "Twitter",
+
+    icon: "",
+},
+
+    
+
+
+]
+
+  return (
+    <div>
+        <h1>YASSIR 👷🏻‍♂️⚡️👨🏻‍💻</h1>
+
+    </div>
+  )
+}
