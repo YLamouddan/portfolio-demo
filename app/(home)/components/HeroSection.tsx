489f8b5c90f4f036df79f1232ce2885b41bc13cc
YLamouddan <165455306+YLamouddan@users.noreply.github.com>
2024-04-03 09:17:01 +0200

    modified:   app/(home)/components/HeroSection.tsx modified:   app/(home)/components/Navbar.tsx modified:   app/(home)/page.tsx modified:   app/globals.css new file:   components/ui/moving-border.tsx modified:   package-lock.json modified:   package.json modified:   tailwind.config.ts

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -1,66 +1,63 @@
-import Link from 'next/link'
-import React from 'react'
+import { MovingBorderBtn } from "@/components/ui/moving-border";
+import Link from "next/link";
+import React from "react";
 
 export default function HeroSection() {
+  return (
+    <div className=" min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between">
+      <div className=" space-y-10 text  text-center lg:text-left">
+        <h1 className=" text-4xl lg:text-7xl font-bold">
+          {" "}
+          Nice to meet you! 👋
+          <br />{" "}
+          <span className=" underline underline-offset-8 decoration-green-500">
+            {"I'm Yassir."}
+          </span>
+        </h1>
 
-    return (
+        <p className=" md:w-96 text-lg text-gray-300">
+          {
+            "Based in Poland,I'm a Web developer and an Electrical Engineer. I'm passionate about building a modern web application that users love"
+          }
+        </p>
 
-        <div className=" min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between">
- 
-            <div className=" space-y-10 text  text-center lg:text-left">
-                <h1 className=" text-4xl lg:text-7xl font-bold"> Nice to meet you! 👋 
+        <Link
+          href={"mailto:Ylamouddan@gmail.com"}
+          className="inline-block group "
+        >
+          <div>
+            <h1 className=" text-3xl font-bold  group-hover:text-green-500 transition-all">
+              {" "}
+              Contact Me 📭
+            </h1>
 
-                <br /> <span className=" underline underline-offset-8 decoration-green-500">{"I'm Yassir."}</span>
-                
-                </h1>
+            <div className=" w-40 h-2 bg-green-500 rounded-full"></div>
 
-                <p className=" md:w-96 text-lg text-gray-300"> 
-                    {
-                    
-                    "Based in Poland,I'm a Web developer and an Electrical Engineer. I'm passionate about building a modern web application that users love"
+            <div className=" w-40 h-2 bg-indigo-500 rounded-full translate-x-2"></div>
+          </div>
+        </Link>
+      </div>
 
-                    } 
-                    
-                </p>
+      <div className="relative">
+        <div className="w-72 h-72 space-y-3 -rotate-[30deg] relative">
+          <div className="flex gap-3 translate-x-8 ">
+            <div className="w-32 h-32 rounded-2xl bg-green-500"></div>
+            <div className="w-32 h-32 rounded-full bg-indigo-500"></div>
+          </div>
 
-                <Link href={"mailto:Ylamouddan@gmail.com"} className="inline-block group ">
+          <div className="flex gap-3 -translate-x-8 ">
+            <div className="w-32 h-32 rounded-2xl bg-indigo-500 "></div>
+            <div className="w-32 h-32 rounded-full bg-green-500"></div>
+          </div>
 
-                    <div>
-
-                        <h1 className=" text-3xl font-bold  group-hover:text-green-500 transition-all"> Contact Me 📭</h1>
-
-                        <div className=" w-40 h-2 bg-green-500 rounded-full"></div>
-
-                        <div className=" w-40 h-2 bg-indigo-500 rounded-full translate-x-2"></div>
-
-                    </div>
-                
-                </Link>
-                    
-            </div>
-
-            <div>
-
-                <div className="w-72 h-72 space-y-3 -rotate-[30deg]">
-                
-                    <div className="flex gap-3 translate-x-8 ">
-
-                        <div className="w-32 h-32 rounded-2xl bg-green-500"></div>
-                        <div className="w-32 h-32 rounded-full bg-indigo-500"></div>
-
-                    </div>
-
-                    <div className="flex gap-3 -translate-x-8 ">
-
-                        <div className="w-32 h-32 rounded-2xl bg-indigo-500 "></div>
-                        <div className="w-32 h-32 rounded-full bg-green-500"></div>
-
-                    </div>
-
-                </div>
-
-            </div>
-
+          <div className="glow absolute top-[40%] right-1/2 -z-10 "></div>
         </div>
-        );
+        <div className="absolute bottom-5 sm:bottom-14 left-0 sm: -left-10">
+          <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
+            <p>📢 Avalivale For Work </p>
+          </MovingBorderBtn>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -1,54 +1,46 @@
-import React from 'react';
-import { SiGithub, SiLinkedin, SiX } from 'react-icons/si';
-import { FaFilePdf } from 'react-icons/fa6';
-import Link from 'next/link';
+import React from "react";
+import { SiGithub, SiLinkedin, SiX } from "react-icons/si";
+import { FaFilePdf } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function Navbar() {
   const socials = [
     {
-      link: 'https://www.Linkedin.com/in/iyassir',
-      label: 'Linkedin',
+      link: "https://www.Linkedin.com/in/iyassir",
+      label: "Linkedin",
       icon: SiLinkedin,
     },
     {
-      link: 'https://github.com/YLamouddan',
-      label: 'Github',
+      link: "https://github.com/YLamouddan",
+      label: "Github",
       icon: SiGithub,
     },
     {
-      link: 'https://twitter.com/YLamouddan',
-      label: 'Twitter',
+      link: "https://twitter.com/YLamouddan",
+      label: "Twitter",
       icon: SiX,
     },
     {
-      link: 'https://YLamouddan.com',
-      label: 'Resume',
+      link: "https://YLamouddan.com",
+      label: "Resume",
       icon: FaFilePdf,
     },
   ];
 
   return (
     <nav className=" py-10 flex justify-between items-center ">
-      <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2">YLmaouddan👷🏻‍♂️⚡️👨🏻‍💻</h1>
+      <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2">
+        YLmaouddan👷🏻‍♂️⚡️👨🏻‍💻
+      </h1>
       <div className="flex items-center gap-5">
-        {socials.map((social,index) => {
+        {socials.map((social, index) => {
+          const Icon = social.icon;
 
-            const Icon = social.icon;
-
-            return(
-
-                <Link
-                    href={social.link}
-
-                    key={index}
-
-                    aria-label={social.label}
-                >
-                    <Icon className="w-5 h-5 hover:scale-125 transition-all"/>
-
-            
-                </Link>
-            );
+          return (
+            <Link href={social.link} key={index} aria-label={social.label}>
+              <Icon className="w-5 h-5 hover:scale-125 transition-all" />
+            </Link>
+          );
         })}
       </div>
     </nav>
